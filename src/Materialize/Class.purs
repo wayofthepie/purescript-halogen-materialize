b1d7b1de37ad2aeb99275ac26a1deeea9881bbbc
wayofthepie <wayofthepie@gmail.com>
2017-05-07 15:00:12 +0100

    Add common classes

--- src/Materialize/Class.purs
+++ src/Materialize/Class.purs
@@ -8,11 +8,46 @@ import Halogen.HTML (ClassName(..))
 btn :: ClassName
 btn = ClassName "btn"
 
+btnFloating :: ClassName
+btnFloating = ClassName "btn-floating"
+
+btnLarge :: ClassName
+btnLarge = ClassName "btn-large"
+
 fixedActionButton :: ClassName
 fixedActionButton = ClassName "fixed-action-btn"
+
+horizontal :: ClassName
+horizontal = ClassName "horizontal"
+
 
 --------------------------------------------------------------------------------
 -- Icons
 --------------------------------------------------------------------------------
 materialIcons :: ClassName
 materialIcons = ClassName "material-icons"
+
+left :: ClassName
+left = ClassName "left"
+
+right :: ClassName
+right = ClassName "right"
+
+large :: ClassName
+large = ClassName "large"
+
+
+--------------------------------------------------------------------------------
+-- Animation
+--------------------------------------------------------------------------------
+wavesEffect :: ClassName
+wavesEffect = ClassName "waves-effect"
+
+wavesLight  :: ClassName
+wavesLight = ClassName "waves-light"
+
+--------------------------------------------------------------------------------
+-- Other
+--------------------------------------------------------------------------------
+disabled :: ClassName
+disabled = ClassName "disabled"
